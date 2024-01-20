2e909b0904a31646713525a8f6fcaf8ed93bd7d1
MelkiyHondavod <nikita.kabeshev@gmail.com>
2024-01-20 15:04:56 +0300

    one slash instead of two

--- custom_logging.py
+++ custom_logging.py
@@ -4,12 +4,12 @@ import time
 def log( string, end='\n', log_file_path="my_data", file_name ="log.txt",     lines_lim = 100 ):
 
 
-    with open( f"{log_file_path}\\{file_name}", 'r+', encoding='utf-8' ) as f:
+    with open( fR"{log_file_path}\{file_name}", 'r+', encoding='utf-8' ) as f:
         
 
         lines = f.readlines()
         if len(lines) > lines_lim :
-            dump_f = open( f"""{log_file_path}\\old_logs\\dump{ int(lines[0]) }.txt""", 'w', encoding='utf-8' )
+            dump_f = open( fR"""{log_file_path}\old_logs\dump{ int(lines[0]) }.txt""", 'w', encoding='utf-8' )
             dump_f.writelines( lines )
             dump_f.close()
             f.seek(0)
