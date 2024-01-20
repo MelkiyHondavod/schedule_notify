1b8b8b8e75fbcc501b3818dea075025d6f078dc0
MelkiyHondavod <nikita.kabeshev@gmail.com>
2024-01-20 16:40:45 +0300

    my_data in path

--- project_path.py
+++ project_path.py
@@ -1,2 +1,8 @@
+import os
+
 class ProjectPath:
-    path = "/code/my_data"#R"C:\\Users\\nikita\\Desktop\\save\\PyLol\\TimeTable11A\\my_data"
+    if "AMVERA" in os.environ :
+        print("path...")
+        path = "my_data" #/code/
+    else:
+        path = "my_data" #R"C:/Users/nikita/Desktop/save/PyLol/TT11A_fizmat/fizmat-schedule/my_data"
